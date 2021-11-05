d07ad230d6895eef74e8d06c3be6150bb33dfb5e
b2210765019 <b2210765019@cs.hacettepe.edu.tr>
2021-11-05 20:11:44 +0300

    Added python file

--- Exercises/1.py
+++ Exercises/1.py
@@ -1,10 +1,9 @@
-import random 
-number = random.randint(1,50)
-guess_number = int(input("Enter your guess: "))
-while guess_number != number:
-    if guess_number < number:
-        print("It is false.Increase your guess.")
-    if guess_number > number:
-        print("It is false.Decrease your guess.")
-    guess_number = int(input("Enter your guess: ")) 
-print("It is true.")
+N = int(input("Enter a number: "))
+sum_of_evens = 0 
+sum_of_odds = 0 
+for i in range(1,N+1):
+    if i%2 == 0:
+        sum_of_evens +=i
+    else:
+        sum_of_odds +=i
+print(sum_of_odds , sum_of_evens / int(N/2)) 
